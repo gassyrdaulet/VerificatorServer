62646947622ba3267acbff666897f154380d117d
Gassyr <gassyrdaulet@gmail.com>
2022-12-03 23:57:44 +0600

    first

---
+++ index.js
@@ -0,0 +1,37 @@
+import mysql from "mysql2/promise";
+import express from "express";
+import bodyParser from "body-parser";
+
+const production = true;
+const dataBaseConfig = {
+  host: "jackmarket.kz",
+  port: "3306",
+  user: "kaspiver0",
+  password: "password",
+  database: "verificator",
+};
+const dataBaseConfigProduction = {
+  host: "localhost",
+  port: "3306",
+  user: "kaspiver0",
+  password: "password",
+  database: "verificator",
+};
+
+const app = express();
+app.get("/stores", async (req, res) => {
+  try {
+    const conn = await mysql.createConnection(
+      production ? dataBaseConfigProduction : dataBaseConfig
+    );
+    const stores = (await conn.query(`SELECT * FROM stores`))[0];
+    res.send(stores);
+    await conn.end();
+  } catch (e) {
+    res.status(500).json({ message: "Error! " + e });
+  }
+});
+
+app.listen(6969, () => {
+  console.log("Go to http://localhost:3000/stores so you can see the data.");
+});
